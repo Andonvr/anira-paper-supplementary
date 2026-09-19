42e026ec900bdcf612fad0297abb4ca6bb2d7759
Andonvr <andonvr@users.noreply.github.com>
2026-09-19 09:45:57 +0000

    testrun

--- anira-web-r/source/install_packages.r
+++ anira-web-r/source/install_packages.r
@@ -3,13 +3,13 @@ R.version.string
 
 # Install packages
 # ggplot2 is part of the tidyverse, so we don't need to install it separately
-install.packages("here", dependencies = TRUE, repos = "https://cloud.r-project.org", Ncpus = 8)
-install.packages("tidyverse", dependencies = TRUE, repos = "https://cloud.r-project.org", Ncpus = 8)
-install.packages("lme4", dependencies = TRUE, repos = "https://cloud.r-project.org", Ncpus = 8)
-install.packages("lmerTest", dependencies = TRUE, repos = "https://cloud.r-project.org", Ncpus = 8)
-install.packages("performance", dependencies = TRUE, repos = "https://cloud.r-project.org", Ncpus = 8)
-install.packages("parameters", dependencies = TRUE, repos = "https://cloud.r-project.org", Ncpus = 8) # two packages are failing here in r-base image but they are not required hopefully (ClassDiscovery and PCDimension)
-install.packages("sjPlot", dependencies = TRUE, repos = "https://cloud.r-project.org", Ncpus = 8)
-install.packages("emmeans", dependencies = TRUE, repos = "https://cloud.r-project.org", Ncpus = 8)
-install.packages("patchwork", dependencies = TRUE, repos = "https://cloud.r-project.org", Ncpus = 8)
-install.packages("pastecs", dependencies = TRUE, repos = "https://cloud.r-project.org", Ncpus = 8)
+install.packages("here", dependencies = TRUE, repos = "https://cloud.r-project.org", Ncpus = 32)
+install.packages("tidyverse", dependencies = TRUE, repos = "https://cloud.r-project.org", Ncpus = 32)
+install.packages("lme4", dependencies = TRUE, repos = "https://cloud.r-project.org", Ncpus = 32)
+install.packages("lmerTest", dependencies = TRUE, repos = "https://cloud.r-project.org", Ncpus = 32)
+install.packages("performance", dependencies = TRUE, repos = "https://cloud.r-project.org", Ncpus = 32)
+install.packages("parameters", dependencies = TRUE, repos = "https://cloud.r-project.org", Ncpus = 32) # two packages are failing here in r-base image but they are not required hopefully (ClassDiscovery and PCDimension)
+install.packages("sjPlot", dependencies = TRUE, repos = "https://cloud.r-project.org", Ncpus = 32)
+install.packages("emmeans", dependencies = TRUE, repos = "https://cloud.r-project.org", Ncpus = 32)
+install.packages("patchwork", dependencies = TRUE, repos = "https://cloud.r-project.org", Ncpus = 32)
+install.packages("pastecs", dependencies = TRUE, repos = "https://cloud.r-project.org", Ncpus = 32)
